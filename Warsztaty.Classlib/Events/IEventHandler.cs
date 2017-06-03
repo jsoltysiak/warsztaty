ea1160c6aab1840d786d6711e2d22d40984f74d5
Janusz Sołtysiak <janusz.soltysiak@makingwaves.com>
2017-06-03 10:42:56 +0200

    Add EventHandler interface

---
+++ Warsztaty.Classlib/Events/IEventHandler.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Warsztaty.Messages.Events
+{
+    public interface IEventHandler<in T> where T : IEvent
+    {
+        Task HandleAsync(T command);
+    }
+}
